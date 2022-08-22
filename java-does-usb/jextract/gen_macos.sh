8f9337f18a02172497117c520e93bc691610fedc
Manuel Bleichenbacher <manuel.bleichenbacher@gmail.com>
2022-08-22 19:54:18 +0200

    jextract on macOS

---
+++ java-does-usb/jextract/gen_macos.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+JEXTRACT=../../../jextract/build/jextract/bin/jextract
+
+# CoreFoundation
+#$JEXTRACT --source --target-package net.codecrete.usb.macos.gen --output ../src/main/java \
+#  -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
+#  --include-function CFUUIDCreateFromUUIDBytes \
+#  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/CoreFoundation.framework/Versions/A/Headers/CoreFoundation.h
+
+## mach_error
+$JEXTRACT --source --target-package net.codecrete.usb.macos.gen --output ../src/main/java \
+  -I /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include \
+  --include-function mach_error \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/mach/mach.h
