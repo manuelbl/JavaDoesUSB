2c8764bb0755142745696f5699e83aec1e62a366
Manuel Bl <manuel.bleichenbacher@gmail.com>
2022-08-23 19:59:12 +0200

    Linux: reduce jextract gen to needed functions/structs

--- java-does-usb/jextract/gen_linux.sh
+++ java-does-usb/jextract/gen_linux.sh
@@ -13,24 +13,30 @@ JEXTRACT=../../../jextract/build/jextract/bin/jextract
 $JEXTRACT --source --output ../src/main/java \
   --header-class-name errno \
   --target-package net.codecrete.usb.linux.gen.errno \
+  --include-function __errno_location \
   /usr/include/errno.h
 
 # ioctl.h
 $JEXTRACT --source --output ../src/main/java \
   --header-class-name ioctl \
   --target-package net.codecrete.usb.linux.gen.ioctl \
+  --include-function ioctl \
   /usr/include/x86_64-linux-gnu/sys/ioctl.h
 
 # fcntl.h
 $JEXTRACT --source --output ../src/main/java \
   --header-class-name fcntl \
   --target-package net.codecrete.usb.linux.gen.fcntl \
+  --include-function open \
+  --include-macro O_CLOEXEC \
+  --include-macro O_RDWR \
   /usr/include/fcntl.h
 
 # unistd.h
 $JEXTRACT --source --output ../src/main/java \
   --header-class-name unistd \
   --target-package net.codecrete.usb.linux.gen.unistd \
+  --include-function close \
   /usr/include/unistd.h
 
 # usbdevice_fs.h
@@ -38,4 +44,10 @@ $JEXTRACT --source --output ../src/main/java \
 $JEXTRACT --source --output ../src/main/java \
   --header-class-name usbdevice_fs \
   --target-package net.codecrete.usb.linux.gen.usbdevice_fs \
+  --include-struct usbdevfs_bulktransfer \
+  --include-struct usbdevfs_ctrltransfer \
+  --include-macro USBDEVFS_CONTROL \
+  --include-macro USBDEVFS_BULK \
+  --include-macro USBDEVFS_CLAIMINTERFACE \
+  --include-macro USBDEVFS_RELEASEINTERFACE \
   /usr/include/linux/usbdevice_fs.h
--- java-does-usb/src/main/java/net/codecrete/usb/linux/gen/unistd/constants$0.java
+++ java-does-usb/src/main/java/net/codecrete/usb/linux/gen/unistd/constants$0.java
@@ -6,55 +6,12 @@ import java.lang.foreign.FunctionDescriptor;
 import java.lang.invoke.MethodHandle;
 class constants$0 {
 
-    static final FunctionDescriptor access$FUNC = FunctionDescriptor.of(Constants$root.C_INT$LAYOUT,
-        Constants$root.C_POINTER$LAYOUT,
-        Constants$root.C_INT$LAYOUT
-    );
-    static final MethodHandle access$MH = RuntimeHelper.downcallHandle(
-        "access",
-        constants$0.access$FUNC
-    );
-    static final FunctionDescriptor faccessat$FUNC = FunctionDescriptor.of(Constants$root.C_INT$LAYOUT,
-        Constants$root.C_INT$LAYOUT,
-        Constants$root.C_POINTER$LAYOUT,
-        Constants$root.C_INT$LAYOUT,
-        Constants$root.C_INT$LAYOUT
-    );
-    static final MethodHandle faccessat$MH = RuntimeHelper.downcallHandle(
-        "faccessat",
-        constants$0.faccessat$FUNC
-    );
-    static final FunctionDescriptor lseek$FUNC = FunctionDescriptor.of(Constants$root.C_LONG_LONG$LAYOUT,
-        Constants$root.C_INT$LAYOUT,
-        Constants$root.C_LONG_LONG$LAYOUT,
-        Constants$root.C_INT$LAYOUT
-    );
-    static final MethodHandle lseek$MH = RuntimeHelper.downcallHandle(
-        "lseek",
-        constants$0.lseek$FUNC
-    );
     static final FunctionDescriptor close$FUNC = FunctionDescriptor.of(Constants$root.C_INT$LAYOUT,
         Constants$root.C_INT$LAYOUT
     );
     static final MethodHandle close$MH = RuntimeHelper.downcallHandle(
         "close",
         constants$0.close$FUNC
-    );
-    static final FunctionDescriptor closefrom$FUNC = FunctionDescriptor.ofVoid(
-        Constants$root.C_INT$LAYOUT
-    );
-    static final MethodHandle closefrom$MH = RuntimeHelper.downcallHandle(
-        "closefrom",
-        constants$0.closefrom$FUNC
-    );
-    static final FunctionDescriptor read$FUNC = FunctionDescriptor.of(Constants$root.C_LONG_LONG$LAYOUT,
-        Constants$root.C_INT$LAYOUT,
-        Constants$root.C_POINTER$LAYOUT,
-        Constants$root.C_LONG_LONG$LAYOUT
-    );
-    static final MethodHandle read$MH = RuntimeHelper.downcallHandle(
-        "read",
-        constants$0.read$FUNC
     );
 }
 
